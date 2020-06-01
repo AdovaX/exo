d64fc31bdd1b89d46621ead1ea7e2f308f3eac87
adovax <mailstodeepakdev@gmail.com>
2020-06-01 16:47:40 +0530

    DB seeded

---
+++ database/seeds/Userseeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Userseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('users')->insert([
+         'name' => 'Exoadmin',
+         'email' => 'deepak.dev@indocosmo.in',
+         'password' => Hash::make('password'),
+     ]);
+    }
+}
