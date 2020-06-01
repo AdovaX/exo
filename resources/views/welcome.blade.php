393e4056bc83d2b308e281e69970540d1a5b12b9
adovax <mailstodeepakdev@gmail.com>
2020-06-01 17:44:41 +0530

    Renamed Laravel to Exo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Exo
                 </div>
 
                 <div class="links">
